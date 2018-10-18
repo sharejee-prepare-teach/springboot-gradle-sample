5ced323fd2b9b610b6282637c4e07ad73446d179
Rith <rithronlkh@gmail.com>
2018-10-18 07:31:10 +0700

    add refector port to 8081 of view

--- src/main/resources/static/js/service/PersonListService.js
+++ src/main/resources/static/js/service/PersonListService.js
@@ -5,7 +5,7 @@ appPerson.factory('PersonListService', ['$http', '$q', function($http, $q){
     return {
 
         fetchAllItems: function() {
-            return $http.get('http://localhost:8080/item/person_list')
+            return $http.get('http://localhost:8081/item/person_list')
                 .then(
                     function(response){
                         return response.data;
